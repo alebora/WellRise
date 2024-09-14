0c457d510d46964464c75c6958f5b1c5a0c5aa6f
alebora <148511093+alebora@users.noreply.github.com>
2024-09-14 12:07:09 -0400

    starter code

--- WellRise.ino
+++ WellRise.ino
@@ -1,7 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include "rgb_lcd.h"
 
+rgb_lcd lcd;
+
+void setup() {
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    // Print a message to the LCD.
+    lcd.print("hello, world!");
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
